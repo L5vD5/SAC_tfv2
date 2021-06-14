6586a72b767613c6a5d7f6e3bc688a8f3155c3fb
l5vd5 <l5vd5@korea.ac.kr>
2021-06-14 16:55:16 +0900

    stabilize SAC

--- model.py
+++ model.py
@@ -15,13 +15,13 @@ def mlp(odim=24, hdims=[64,64], actv='relu', output_actv=None):
     return layers
 
 class MLPGaussianPolicy(tf.keras.Model):    # def mlp_gaussian_policy
-    def __init__(self, odim, adim, hdims=[64,64], actv='relu', output_actv=None):
+    def __init__(self, odim, adim, hdims=[64,64], actv='relu'):
         super(MLPGaussianPolicy, self).__init__()
         self.net = mlp(odim, hdims, actv, output_actv=actv) #feature
         # mu layer
-        self.mu = tf.keras.layers.Dense(adim, activation=None)
+        self.mu = tf.keras.layers.Dense(adim, activation=None, trainable=True)
         # std layer
-        self.log_std = tf.keras.layers.Dense(adim, activation=None)
+        self.log_std = tf.keras.layers.Dense(adim, activation=None, trainable=True)
 
     @tf.function
     def call(self, o, deterministic=False, get_logprob=True):
@@ -30,7 +30,7 @@ class MLPGaussianPolicy(tf.keras.Model):    # def mlp_gaussian_policy
         log_std = self.log_std(net_ouput)
 
         LOG_STD_MIN, LOG_STD_MAX = -10.0, +2.0
-        log_std = tf.clip_by_value(log_std, LOG_STD_MIN, LOG_STD_MAX) #log_std        # gaussian likelihood
+        log_std = tf.clip_by_value(log_std, LOG_STD_MIN, LOG_STD_MAX) #log_std
         std = tf.exp(log_std) # std
 
         # Pre-squash distribution and sample
@@ -46,7 +46,7 @@ class MLPGaussianPolicy(tf.keras.Model):    # def mlp_gaussian_policy
             # of where it comes from, check out the original SAC paper (arXiv 1801.01290)
             # and look in appendix C. This is a more numerically-stable equivalent to Eq 21.
             # Try deriving it yourself as a (very difficult) exercise. :)
-            logp_pi = tf.reduce_sum(dist.log_prob(pi),axis=-1)    #gaussian log_likelihood # modified axis
+            logp_pi = tf.reduce_sum(dist.log_prob(pi), axis=-1)    #gaussian log_likelihood # modified axis
             logp_pi -= tf.reduce_sum(2 * (np.log(2) - pi - tf.nn.softplus(-2 * pi)), axis=1)
         else:
             logp_pi = None
@@ -56,7 +56,7 @@ class MLPGaussianPolicy(tf.keras.Model):    # def mlp_gaussian_policy
 
 # Q-function mlp
 class MLPQFunction(tf.keras.Model):
-    def __init__(self,odim, adim, hdims=[64, 64], actv='relu'):
+    def __init__(self, odim, adim, hdims=[64, 64], actv='relu'):
         super().__init__()
         self.q = mlp(odim+adim, hdims=hdims+[1], actv=actv, output_actv=None)
 
@@ -64,7 +64,7 @@ class MLPQFunction(tf.keras.Model):
     def call(self, o, a):
         x = tf.concat([o, a], -1)
         q = self.q(x)
-        return tf.squeeze(q, 1)   #Critical to ensure q has right shape.
+        return tf.squeeze(q, axis=1)   #Critical to ensure q has right shape.
 
 
 class MLPActorCritic(tf.keras.Model):   # def mlp_actor_critic
@@ -87,6 +87,7 @@ class MLPActorCritic(tf.keras.Model):   # def mlp_actor_critic
         q1_pi = self.q1(o,pi)
         q2_pi = self.q2(o,pi)
         min_q_pi = tf.minimum(q1_pi, q2_pi)
+        # tf.print('logp_pi', logp_pi)
         # pi losses
         pi_loss = tf.reduce_mean(alpha_pi*logp_pi - min_q_pi)
         return pi_loss
@@ -106,6 +107,7 @@ class MLPActorCritic(tf.keras.Model):   # def mlp_actor_critic
         q_backup = tf.stop_gradient(r + gamma*(1 - d)*(min_q_targ - alpha_q*logp_pi_next))
         q1 = self.q1(o, a)
         q2 = self.q2(o, a)
+        # tf.print('q1', q1, 'q2', q2)
         # value(q) loss
         q1_loss = 0.5*tf.losses.mse(q1,q_backup)           #0.5 * ((q_backup-q1)**2).mean()
         q2_loss = 0.5*tf.losses.mse(q2,q_backup)          #0.5 * ((q_backup-q2)**2).mean()
